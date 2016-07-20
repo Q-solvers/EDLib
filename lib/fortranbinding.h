7cb09b19a05a767bdb7f5b140b047cd99a9c57c9
iskakoff <sir.iskakoff@gmail.com>
2016-07-20 12:37:41 -0400

    ARPACK binding

--- lib/fortranbinding.h
+++ lib/fortranbinding.h
@@ -14,10 +14,10 @@ extern "C" {
 void darnoldi_(int* nloc2,double* vout,double *eout, int*ncv,double* Hstate0, int*nev,int*ierr,int*info);
 
 };
+#endif
 
 void darnoldi(int& nloc,std::vector<double>& vout,std::vector<double>& eout, int& ncv,double& Hstate0, int& nev,int& ierr,int& info) {
   darnoldi_(&nloc,&vout[0],&eout[0], &ncv,&Hstate0, &nev,&ierr,&info);
 }
-#endif
 
 #endif //HUBBARD_FORTRANBINDING_H
