2ce76150cdf145c211389bc04326d2e94c231ec4
Michael Danilov <mike402>
2016-11-07 19:54:53 +0100

    HubbardModel: add magnetic field. SRS test: add MPI ifdefs.

--- test/SRS.cpp
+++ test/SRS.cpp
@@ -1,12 +1,19 @@
 
 #include <gtest/gtest.h>
+
+#ifdef USE_MPI
 #include <mpi.h>
+#endif
 
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
+#ifdef USE_MPI
   MPI_Init(&argc, &argv);
+#endif
   int res = RUN_ALL_TESTS();
+#ifdef USE_MPI
   MPI_Finalize();
+#endif
   return res;
 }
