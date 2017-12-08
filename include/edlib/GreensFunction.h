33a9474a334ebd3477d8a3085c726b0f014f1038
iskakoff <sir.iskakoff@gmail.com>
2017-12-08 16:49:54 -0500

    off-diagonal GF symmetrization fixed

--- include/edlib/GreensFunction.h
+++ include/edlib/GreensFunction.h
@@ -48,6 +48,7 @@ namespace EDLib {
         }
         std::string input = p["INPUT_FILE"];
         alps::hdf5::archive input_file(input.c_str(), "r");
+        std::vector<std::vector<int>> gf_orbs;
         if(input_file.is_data("GreensFunction_orbitals/values")){
           input_file >> alps::make_pvp("GreensFunction_orbitals/values", gf_orbs);
         }else{
@@ -59,19 +60,19 @@ namespace EDLib {
         }
         input_file.close();
         // Find all unique indices for the diagonal part.
-        diagonal_orbs.resize(gf_orbs.size() * 2);
+        _diagonal_orbs.resize(gf_orbs.size() * 2);
         for(int i = 0; i < gf_orbs.size(); ++i){
          for(int j = 0; j < 2; ++j){
-           diagonal_orbs[i + j * gf_orbs.size()] = gf_orbs[i][j];
+           _diagonal_orbs[i + j * gf_orbs.size()] = gf_orbs[i][j];
          }
         }
-        std::sort(diagonal_orbs.begin(), diagonal_orbs.end());
-        diagonal_orbs.erase(std::unique(diagonal_orbs.begin(), diagonal_orbs.end()), diagonal_orbs.end());
+        std::sort(_diagonal_orbs.begin(), _diagonal_orbs.end());
+        _diagonal_orbs.erase(std::unique(_diagonal_orbs.begin(), _diagonal_orbs.end()), _diagonal_orbs.end());
         // Find offdiagonal indices.
-        offdiagonal_orbs.clear();
+        _offdiagonal_orbs.clear();
         for(int i = 0; i < gf_orbs.size(); ++i){
           if(gf_orbs[i][0] != gf_orbs[i][1]){
-            offdiagonal_orbs.push_back(gf_orbs[i]);
+            _offdiagonal_orbs.push_back({{size_t(gf_orbs[i][0]),size_t(gf_orbs[i][1])}});
           }
         }
       }
@@ -141,7 +142,7 @@ namespace EDLib {
         MPI_Comm_rank(hamiltonian().storage().comm(), &rank);
         if(rank == 0) {
 #endif
-          if(diagonal_orbs.size() > 0){
+          if(_diagonal_orbs.size() > 0){
             gf.save(ar, path + "/G_omega" + suffix());
             std::ostringstream Gomega_name;
             Gomega_name << "G_omega"<<suffix();
@@ -151,7 +152,7 @@ namespace EDLib {
           }
           std::cout << "Statsum: " << _Z << std::endl;
           ar[path + "/@Statsum"] << _Z;
-          if(offdiagonal_orbs.size() > 0){
+          if(_offdiagonal_orbs.size() > 0){
             std::ostringstream Gomega_name2;
             Gomega_name2 << "G_ij_omega"<<suffix();
             std::ofstream G_omega_file2(Gomega_name2.str().c_str());
@@ -214,15 +215,15 @@ namespace EDLib {
         MPI_Comm_rank(hamiltonian().storage().comm(), &rank);
 #endif
         /// iterate over orbitals for the diagonal Green's function
-        for (int iorb = 0; iorb < diagonal_orbs.size(); ++iorb) {
+        for (int iorb = 0; iorb < _diagonal_orbs.size(); ++iorb) {
           /// iterate over spins
           for (int ispin = 0; ispin < _model.spins(); ++ispin) {
-            int orb = diagonal_orbs[iorb];
+            int orb = _diagonal_orbs[iorb];
             std::vector < precision > outvec(1, precision(0.0));
             precision expectation_value = 0.0;
             _model.symmetry().set_sector(pair.sector());
             /// first we are going to create particle and compute contribution to Green's function
-            if (create_particle(orb, ispin, pair.eigenvector(), outvec, expectation_value)) {
+            if (create_particles(std::array<size_t, 1>{{size_t(orb)}}, ispin, pair.eigenvector(), outvec, expectation_value)) {
               /// Perform Lanczos factorization for starting vector |outvec>
               int nlanc = lanczos(outvec);
 #ifdef USE_MPI
@@ -237,7 +238,7 @@ namespace EDLib {
             /// restore symmetry sector
             _model.symmetry().set_sector(pair.sector());
             /// perform the same for destroying of a particle
-            if (annihilate_particle(orb, ispin, pair.eigenvector(), outvec, expectation_value)) {
+            if (annihilate_particles(std::array<size_t, 1>{{size_t(orb)}}, ispin, pair.eigenvector(), outvec, expectation_value)) {
               int nlanc = lanczos(outvec);
 #ifdef USE_MPI
               if(!rank)
@@ -262,28 +263,16 @@ namespace EDLib {
         int rank;
         MPI_Comm_rank(hamiltonian().storage().comm(), &rank);
 #endif
-        for (int iorb = 0; iorb < offdiagonal_orbs.size(); ++iorb) {
+        for (int iorb = 0; iorb < _offdiagonal_orbs.size(); ++iorb) {
           for (int ispin = 0; ispin < _model.spins(); ++ispin) {
-            std::vector<int> orbs = offdiagonal_orbs[iorb];
-            std::vector<std::vector<precision>> outvec(2, std::vector<precision>(1, precision(0.0)));
-            std::vector < precision > sumvec(1, precision(0.0));
+            auto orbs = _offdiagonal_orbs[iorb];
+            std::vector < precision > outvec(1, precision(0.0));
             bool found[2];
             precision expectation_value = 0.0;
             /// create particle on two different orbs, sum the resulting vectors and compute contribution to Green's function
-            for(int i = 0; i < 2; ++i){
-              _model.symmetry().set_sector(pair.sector());
-              found[i] = create_particle(orbs[i], ispin, pair.eigenvector(), outvec[i], expectation_value);
-            }
-            if(found[0] || found[1]){
-              if(found[0] && found[1]){
-               sumvec = outvec[0];
-               for(size_t i = 0; i < sumvec.size(); ++i){
-                 sumvec[i] += outvec[1][i];
-               }
-              }else{
-                sumvec = (found[0] ? outvec[0] : outvec[1]);
-              }
-              int nlanc = lanczos(sumvec);
+            _model.symmetry().set_sector(pair.sector());
+            if(create_particles(orbs, ispin, pair.eigenvector(), outvec, expectation_value)) {
+              int nlanc = lanczos(outvec);
 #ifdef USE_MPI
               if(!rank)
 #endif
@@ -293,21 +282,10 @@ namespace EDLib {
               }
             }
             /// perform the same for destroying of a particle
-            for(int i = 0; i < 2; ++i){
-              /// restore symmetry sector
-              _model.symmetry().set_sector(pair.sector());
-              found[i] = annihilate_particle(orbs[i], ispin, pair.eigenvector(), outvec[i], expectation_value);
-            }
-            if(found[0] || found[1]){
-              if(found[0] && found[1]){
-               sumvec = outvec[0];
-               for(size_t i = 0; i < sumvec.size(); ++i){
-                 sumvec[i] += outvec[1][i];
-               }
-              }else{
-                sumvec = (found[0] ? outvec[0] : outvec[1]);
-              }
-              int nlanc = lanczos(sumvec);
+            /// restore symmetry sector
+            _model.symmetry().set_sector(pair.sector());
+            if(annihilate_particles(orbs, ispin, pair.eigenvector(), outvec, expectation_value) ) {
+              int nlanc = lanczos(outvec);
 #ifdef USE_MPI
               if(!rank)
 #endif
@@ -328,9 +306,9 @@ namespace EDLib {
        */
       void non_local_gf() {
         for (int iomega = 0; iomega < omega().extent(); ++iomega) {
-          for (int iorb = 0; iorb < offdiagonal_orbs.size(); ++iorb) {
+          for (int iorb = 0; iorb < _offdiagonal_orbs.size(); ++iorb) {
             for (int ispin = 0; ispin < _model.spins(); ++ispin) {
-              std::vector<int> orbs = offdiagonal_orbs[iorb];
+              auto orbs = _offdiagonal_orbs[iorb];
               for (int j = 0; j < 2; ++j) {
                 gf_ij(frequency_mesh_index(iomega), index_mesh_index(_model.interacting_orbitals() * orbs[0] + orbs[1]), index_mesh_index(ispin)) -= gf(frequency_mesh_index(iomega), index_mesh_index(orbs[j]), index_mesh_index(ispin));
               }
@@ -353,39 +331,40 @@ namespace EDLib {
       precision _cutoff;
       /// Statsum
       precision _Z;
-      /// Orbital pairs to calculate the Green's function.
-      std::vector<std::vector<int>> gf_orbs;
       /// Orbitals to calculate the diagonal Green's function.
-      std::vector<int> diagonal_orbs;
+      std::vector<int> _diagonal_orbs;
       /// Orbital pairs to calculate the offdiagonal Green's function.
-      std::vector<std::vector<int>> offdiagonal_orbs;
+      std::vector<std::array<size_t, 2> > _offdiagonal_orbs;
 
       /**
        * @brief Perform the create operator action to the eigenstate
        *
-       * @param orbital - the orbital to create a particle
+       * @param orbitals - the orbital to create a particle
        * @param spin - the spin of a particle to create
        * @param invec - current eigenstate
        * @param outvec - Op-vec product
        * @param expectation_value - expectation value of aa*
        * @return true if the particle has been created
        */
-      bool create_particle(int orbital, int spin, const std::vector < precision > &invec, std::vector < precision > &outvec, double &expectation_value) {
+      template<size_t N>
+      bool create_particles(std::array<size_t, N> orbitals, int spin, const std::vector<precision> &invec, std::vector<precision> &outvec, double &expectation_value) {
         // check that the particle can be annihilated
         if (!_model.symmetry().can_create_particle(spin)) {
           return false;
         }
         hamiltonian().storage().reset();
-        long long k = 0;
-        int sign = 0;
         int nup_new = _model.symmetry().sector().nup() + (1 - spin);
         int ndn_new = _model.symmetry().sector().ndown() + spin;
 
         typename Hamiltonian::ModelType::Sector next_sec = _model.symmetry().create_partice(spin);
         outvec.assign(hamiltonian().storage().vector_size(next_sec), 0.0);
-        int i = 0;
         common::statistics.registerEvent("adag");
-        hamiltonian().storage().a_adag(orbital + spin * _model.orbitals(), invec, outvec, next_sec, false);
+        for(auto orb : orbitals) {
+          _model.symmetry().init();
+          std::vector<precision> tmpout(outvec.size());
+          hamiltonian().storage().a_adag(orb + spin * _model.orbitals(), invec, tmpout, next_sec, false);
+          std::transform(tmpout.begin(), tmpout.end(), outvec.begin(), outvec.begin(), std::plus<precision>());
+        }
         common::statistics.updateEvent("adag");
         double norm = hamiltonian().storage().vv(outvec, outvec);
         for (int j = 0; j < outvec.size(); ++j) {
@@ -399,28 +378,31 @@ namespace EDLib {
       /**
        * @brief Perform the annihilator operator action to the eigenstate
        *
-       * @param orbital - the orbital to destroy a particle
+       * @param orbitals - the orbitals list to destroy a particle
        * @param spin - the spin of a particle to destroy
        * @param invec - current eigenstate
        * @param outvec - Op-vec product
        * @param expectation_value - expectation value of a*a
        * @return true if the particle has been destroyed
        */
-      bool annihilate_particle(int orbital, int spin, const std::vector < precision > &invec, std::vector < precision > &outvec, double &expectation_value) {
+      template<size_t N>
+      bool annihilate_particles(std::array<size_t, N> orbitals, int spin, const std::vector<precision> &invec, std::vector<precision> &outvec, double &expectation_value) {
         // check that the particle can be annihilated
         if (!_model.symmetry().can_destroy_particle(spin)) {
           return false;
         }
         hamiltonian().storage().reset();
-        long long k = 0;
-        int sign = 0;
         int nup_new = _model.symmetry().sector().nup() - (1 - spin);
         int ndn_new = _model.symmetry().sector().ndown() - spin;
         typename Hamiltonian::ModelType::Sector next_sec = _model.symmetry().destroy_partice(spin);
         outvec.assign(hamiltonian().storage().vector_size(next_sec), precision(0.0));
-        int i = 0;
         common::statistics.registerEvent("a");
-        hamiltonian().storage().a_adag(orbital + spin * _model.orbitals(), invec, outvec, next_sec, true);
+        for(auto orb : orbitals) {
+          _model.symmetry().init();
+          std::vector<precision> tmpout(outvec.size());
+          hamiltonian().storage().a_adag(orb + spin * _model.orbitals(), invec, tmpout, next_sec, true);
+          std::transform(tmpout.begin(), tmpout.end(), outvec.begin(), outvec.begin(), std::plus<precision>());
+        }
         common::statistics.updateEvent("a");
         double norm = hamiltonian().storage().vv(outvec, outvec);
         for (int j = 0; j < outvec.size(); ++j) {
