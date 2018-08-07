e2b5ed17785c063ac00235869575c3d4e1376f81
Michael Danilov <mike402>
2018-08-07 14:37:42 +0200

    HubbardModel: make magnetic field per-site.

--- test/input/4ring/generate_input.py
+++ test/input/4ring/generate_input.py
@@ -4,6 +4,7 @@ import h5py
 import numpy as np
 
 U = np.array([5.0, 5.0, 5.0, 5.0])
+H = np.array([0.01, 0.01, 0.01, 0.01])
 xmu = np.array([2.5, 2.5, 2.5, 2.5])
 t = np.array([[ 0.0, -1.0,  0.0, -1.0],
               [-1.0,  0.0, -1.0,  0.0],
@@ -27,7 +28,8 @@ hop_g.create_dataset("values", data=t)
 int_g = data.create_group("interaction")
 int_ds = int_g.create_dataset("values", shape=(Ns,), data=U)
 
-int_g = data.create_group("chemical_potential")
-int_ds = int_g.create_dataset("values", shape=(Ns,), data=xmu)
+chem_g = data.create_group("chemical_potential")
+chem_ds = chem_g.create_dataset("values", shape=(Ns,), data=xmu)
 
-data["magnetic_field"] = 0.01
+mag_g = data.create_group("magnetic_field")
+mag_ds = mag_g.create_dataset("values", shape=(Ns,), data=H)
