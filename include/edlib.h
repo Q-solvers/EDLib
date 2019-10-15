5c9366cf7f292f32f1fe38484dcc490805f7f34c
iskakoff <sir.iskakoff@gmail.com>
2019-10-15 18:04:06 -0400

    install targets

---
+++ include/edlib.h
@@ -0,0 +1,18 @@
+#ifndef EDLIB_H
+#define EDLIB_H
+
+#include <edlib/EDParams.h>
+#include "edlib/Hamiltonian.h"
+#include "edlib/SzSymmetry.h"
+#include "edlib/SOCRSStorage.h"
+#include "edlib/CRSStorage.h"
+#include "edlib/HubbardModel.h"
+#include "edlib/GreensFunction.h"
+#include "edlib/ChiLoc.h"
+#include "edlib/HDF5Utils.h"
+#include "edlib/SpinResolvedStorage.h"
+#include "edlib/StaticObservables.h"
+#include "edlib/MeshFactory.h"
+#include "edlib/ExecutionStatistic.h"
+
+#endif //EDLIB_H
