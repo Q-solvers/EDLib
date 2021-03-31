1e541793a41e436fa3c9f4349b14ebb7e1e928dc
Michael Danilov <mike402@users.noreply.github.com>
2021-03-31 23:07:59 +0200

    Calculation of <14|c_i_up c_j_down|16>

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@
 #include "edlib/SpinResolvedStorage.h"
 #include "edlib/StaticObservables.h"
 #include "edlib/MeshFactory.h"
+#include "edlib/EigenPair.h"
 
 // freq, I, J, spin
 template<typename freq_grid>
@@ -50,56 +51,66 @@ int main(int argc, const char ** argv) {
     HamType ham(params);
 #endif
     ham.diag();
-    EDLib::StaticObservables<HamType> so(params);
-    std::ofstream so_out("static_observables");
-    so.print_static_observables(ham, so_out);
-    for (const auto& pair :ham.eigenpairs()) {
-      so.print_major_electronic_configuration(ham, pair, 256, 1e-5, so_out);
-      so.print_class_contrib(ham, pair, 256, 1e-5, true, so_out);
-    }
-    so_out.close();
-    EDLib::hdf5::save_eigen_pairs(ham, ar, "results");
     EDLib::gf::GreensFunction < HamType, EDLib::MatsubaraMeshFactory, alps::gf::statistics::statistics_type> greensFunction(params, ham,alps::gf::statistics::statistics_type::FERMIONIC);
-    //EDLib::gf::GreensFunction < HamType, alps::gf::real_frequency_mesh> greensFunction(params, ham);
-    greensFunction.compute();
-    greensFunction.save(ar, "results");
-    EDLib::gf::ChiLoc<HamType, EDLib::MatsubaraMeshFactory, alps::gf::statistics::statistics_type> susc(params, ham, alps::gf::statistics::statistics_type::BOSONIC);
-    //EDLib::gf::ChiLoc< HamType, alps::gf::real_frequency_mesh> susc(params, ham);
-    susc.compute();
-    susc.save(ar, "results");
-    susc.compute<EDLib::gf::NOperator<double> >();
-    susc.save(ar, "results");
-    
-    auto & G_ij = greensFunction.G_ij();
-    auto sigma= greensFunction.compute_selfenergy(ar, "results");
-    alps::gf::real_space_index_mesh r_mesh(ham.model().interacting_orbitals(), 2);
-    gf_type<alps::gf::matsubara_positive_mesh> cluster_gf(G_ij.mesh1(), r_mesh, r_mesh, G_ij.mesh3());
-    gf_type<alps::gf::matsubara_positive_mesh> cluster_sigma(G_ij.mesh1(), r_mesh, r_mesh, G_ij.mesh3());
-    for(int iw = 0 ; iw<G_ij.mesh1().extent(); ++iw) {
-      for(int is = 0; is<G_ij.mesh3().extent(); ++is) {
-        for(int i = 0; i < G_ij.mesh2().extent(); ++i) {
-          int I = i / r_mesh.extent();
-          int J = i % r_mesh.extent();
-          cluster_gf(mat_mesh_t::index_type(iw), r_mesh_t::index_type(I), r_mesh_t::index_type(J), s_mesh_t::index_type(is)) = 
-              G_ij(mat_mesh_t::index_type(iw), s_mesh_t::index_type(i), s_mesh_t::index_type(is));
-          cluster_sigma(mat_mesh_t::index_type(iw), r_mesh_t::index_type(I), r_mesh_t::index_type(J), s_mesh_t::index_type(is)) = 
-              sigma(mat_mesh_t::index_type(iw), s_mesh_t::index_type(i), s_mesh_t::index_type(is));
-        }
+    size_t count = 0;
+    std::vector<size_t> N(2, 0);
+    size_t Nmax = 0;
+    auto pair1i = ham.eigenpairs().begin();
+    auto pair2i = ham.eigenpairs().begin();
+    for(auto ipair = ham.eigenpairs().begin(); ipair != ham.eigenpairs().end(); ++ipair){
+      const auto& pair = *ipair;
+      if(count){
+        pair2i = ipair;
       }
+      N[count] = pair.sector().nup() + pair.sector().ndown();
+      if(N[count] > Nmax){
+       Nmax = N[count];
+      }
+      ++count;
     }
+    const auto& pair1 = *pair1i;
+    const auto& pair2 = *pair2i;
+    std::vector<double> outvec(1, double(0.0));
+    std::vector<double> outvec2(1, double(0.0));
+    double expectation_value = 0.0;
+    for(size_t ispin = 0; ispin < ham.model().spins(); ++ispin){
+      std::ostringstream cc_name;
+      cc_name << "cc_" << ispin << ".txt";
+      std::ofstream cc_out(cc_name.str().c_str());
+      for(size_t orb1 = 0; orb1 < ham.model().interacting_orbitals(); ++orb1){
+        for(size_t orb2 = 0; orb2 < ham.model().interacting_orbitals(); ++orb2){
+          if(N[1] > N[0]){
+            double product = 0.0;
+            ham.model().symmetry().set_sector(pair2.sector());
+            if(greensFunction.annihilate_particles(std::array<size_t, 1>{{size_t(orb1)}}, ispin, pair2.eigenvector(), outvec2, expectation_value)) {
+              if(greensFunction.annihilate_particles(std::array<size_t, 1>{{size_t(orb2)}}, (1 - ispin), outvec2, outvec, expectation_value)) {
+                product = ham.storage().vv(pair1.eigenvector(), outvec
 #ifdef USE_MPI
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  if(!rank) 
+                  , ham.comm()
 #endif
-    ar.close();
+                );
+              }
+            }
+            cc_out << product << "\t";
+          }else{
+            double product = 0.0;
+            ham.model().symmetry().set_sector(pair1.sector());
+            if(greensFunction.annihilate_particles(std::array<size_t, 1>{{size_t(orb1)}}, ispin, pair1.eigenvector(), outvec2, expectation_value)) {
+              if(greensFunction.annihilate_particles(std::array<size_t, 1>{{size_t(orb2)}}, (1 - ispin), outvec2, outvec, expectation_value)) {
+                product = ham.storage().vv(pair2.eigenvector(), outvec
 #ifdef USE_MPI
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  if(!rank) 
+                  , ham.comm()
 #endif
-    ar.open(params["CLUSTER_DATA"].as<std::string>().c_str(), "w");
-    ar["G_ij"]<<cluster_gf;
-    ar["Sigma_ij"]<<cluster_sigma;
-//    EDLib::CSRSIAMHamiltonian ham2(params);
+                );
+              }
+            }
+            cc_out << product << "\t";
+          }
+        }
+        cc_out << std::endl;
+      }
+      cc_out.close();
+    }
   } catch (std::exception & e) {
 #ifdef USE_MPI
     if(!rank) std::cerr<<e.what()<<std::endl;
