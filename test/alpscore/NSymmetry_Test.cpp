06896739403bcd7f684fcfab4e94bd3e6a8bfc0b
iskakoff <sir.iskakoff@gmail.com>
2026-05-16 17:41:58 -0400

    base library should not have Core prefix, alpscore based tests are moved to alpsocre subdir

---
+++ test/alpscore/NSymmetry_Test.cpp
@@ -0,0 +1,22 @@
+//
+// Created by iskakoff on 21/08/16.
+//
+
+#include "gtest/gtest.h"
+
+#include "edlib/alpscore/NSymmetry.h"
+#include "edlib/alpscore/EDParams.h"
+
+TEST(NSymmetryTest, Initialization) {
+  alps::params p;
+  EDLib::define_parameters(p);
+  EDLib::Symmetry::NSymmetry sym(p);
+  while(sym.next_sector()) {
+    sym.init();
+    int i = 0;
+    while (sym.next_state()) {
+      ASSERT_EQ(i, sym.index(sym.state()));
+      ++i;
+    }
+  }
+}
---
+++ test/alpscore/SzSymmetry_Test.cpp
@@ -0,0 +1,40 @@
+//
+// Created by iskakoff on 22/07/16.
+//
+
+#include "gtest/gtest.h"
+
+#include "edlib/alpscore/SzSymmetry.h"
+#include "edlib/alpscore/EDParams.h"
+
+
+TEST(SzSymmetryTest, Combinatorics) {
+  alps::params p;
+  EDLib::define_parameters(p);
+  EDLib::Symmetry::SzSymmetry sym(p);
+  sym.init();
+
+  ASSERT_EQ(sym.comb().c_n_k(3, 2), 3);
+}
+
+
+TEST(SzSymmetryTest, States) {
+  alps::params p;
+  EDLib::define_parameters(p);
+  EDLib::Symmetry::SzSymmetry sym(p);
+  sym.init();
+}
+
+TEST(SzSymmetryTest, Initialization) {
+  alps::params p;
+  EDLib::define_parameters(p);
+  EDLib::Symmetry::SzSymmetry sym(p);
+  while(sym.next_sector()) {
+    sym.init();
+    int i = 0;
+    while (sym.next_state()) {
+      ASSERT_EQ(i, sym.index(sym.state()));
+      ++i;
+    }
+  }
+}
